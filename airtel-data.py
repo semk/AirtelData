3c4dc5fa009546cfd1bbf1db4a5c46100c073828
Sreejith Kesavan <sreejithemk@gmail.com>
2013-01-21 01:19:25 +0530

    Initial commit

---
+++ airtel-data.py
@@ -0,0 +1,72 @@
+#! /usr/bin/env python
+#
+# Script to display Airtel Broadband usage statistics
+#
+# @author: Sreejith K <sreejithemk@gmail.com>
+# Created On Jan 21 2013
+
+
+import urllib2
+from HTMLParser import HTMLParser
+
+
+USAGE_URL = 'http://122.160.230.125:8080/gbod/'
+
+
+class AirtelDataUsageParser(HTMLParser):
+    """Parses the Data usage from the SmartBytes HTML page.
+    """
+    def __init__(self):
+        self._inbody = False
+        self._data_ready = False
+        self._usage_info_ready = False
+        self._usage_info = []
+        HTMLParser.__init__(self)
+        self.get_page_info()
+
+    def get_page_info(self):
+        """Get the HTML data.
+        """
+        response = urllib2.urlopen(USAGE_URL)
+        data = response.read()
+        self.feed(data)
+
+    def print_usage(self):
+        """Print the data usage statistics.
+        """
+        dsl_number = self._usage_info[:2]
+        balance_quota = self._usage_info[2:5]
+        high_speed_limit = self._usage_info[5:8]
+        days_left = self._usage_info[8:10]
+        print ' '.join(dsl_number)
+        print ' '.join(balance_quota)
+        print ' '.join(high_speed_limit)
+        print ' '.join(days_left)
+
+    def handle_starttag(self, tag, attrs):
+        if self._data_ready and tag == 'li':
+            self._usage_info_ready = True
+        elif self._inbody and tag == 'div'\
+                          and attrs == [('class', 'content-data')]:
+            self._data_ready = True
+        elif tag == 'body':
+            self._inbody = True
+
+    def handle_endtag(self, tag):
+        if self._data_ready and tag == 'div':
+            self._data_ready = False
+        if self._usage_info_ready and tag == 'ul':
+            self._usage_info_ready = False
+
+    def handle_data(self, data):
+        if self._usage_info_ready and not data.isspace():
+            self._usage_info.append(data)
+
+
+def print_usage():
+    parser = AirtelDataUsageParser()
+    parser.print_usage()
+
+
+if __name__ == '__main__':
+    print_usage()
